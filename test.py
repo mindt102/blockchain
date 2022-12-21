4a00018d565051a2b66442a08568eb3c91c231be
Nguyen Viet Hung <vhungpmpx@gmail.com>
2022-12-22 01:05:37 +0700

    Done compute_merkle_root_

--- test.py
+++ test.py
@@ -21,18 +21,35 @@ for _ in range(1):
 
 print(transactions)
 
+def merkle_parent(arg0, arg1):
+    hash0 = bytes.fromhex(arg0)
+    hash1 = bytes.fromhex(arg1)
+    parent = hash256(hash0+hash1)
+    print("Parent hex: ", parent.hex())
+    return parent
 
 def compute_merkle_root(transactions: list[Transaction]) -> bytes:
-    '''Compute the merkle root of a list of transactions
-    Args:
-        transactions (list[Transaction]): list of transactions
-    Returns:
-        bytes: merkle root
-    '''
-    if len(transactions) == 1:
+    #Compute the merkle root of a list of transactions
+    #Args:
+    #    transactions (list[Transaction]): list of transactions
+    #Returns:
+    #    bytes: merkle root
+    if len(transactions) != 1:
         merkle_root = hash256(transactions[0].serialize())
     else:
         # TODO: Edit this to compute the merkle root
+        hex_hashes = []
+        for _ in range(len(transactions)):
+            hash_transac = transactions[_].get_hash()
+            hex_hashes.append(hash_transac.hex())
+        if len(transactions) % 2 == 1:
+                last_hash_transac = transactions[-1].get_hash()
+                hex_hashes.append(last_hash_transac.hex())
+        Merkle_tree = []
+        #while len(hex_hashes) >= 2:
+        for i in range(0, len(hex_hashes), 2):
+            Merkle_tree.append(merkle_parent(hex_hashes[i], hex_hashes[i+1]))
+        merkle_root = Merkle_tree[0]
         pass
     return merkle_root
 

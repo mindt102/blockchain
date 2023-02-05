bf1293f1b39cb1a0219d48bfb1754d55787e189c
Ubuntu <ubuntu@ip-172-31-38-63.ap-southeast-1.compute.internal>
2023-02-05 03:52:38 +0000

    add current difficulty

--- difficulty_adjustment.py
+++ difficulty_adjustment.py
@@ -9,14 +9,28 @@ def get_bits():
     db_name = "./data/bc.db"
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
+
+    # Get the bits history
     query = "SELECT height, bits, timestamp FROM block_headers GROUP BY bits ORDER BY height ASC"
     cursor.execute(query)
     conn.commit()
-    return cursor.fetchall()
+    bits_history = cursor.fetchall()
+
+    # Get the current bits
+    query_current = "SELECT height, bits FROM block_headers ORDER BY height DESC LIMIT 1"
+    bits_current = cursor.execute(query_current)
+    conn.commit()
+    bits_current = bits_current.fetchone()
+
+    return bits_history, bits_current
 
 
-df = pd.DataFrame(get_bits(), columns=["height", "bits", "timestamp"])
+history, current = get_bits()
+
+
+df = pd.DataFrame(history, columns=["height", "bits", "timestamp"])
 df["target"] = df["bits"].apply(bits_to_target)
+
 
 periods = pd.DataFrame()
 periods["time"] = df["timestamp"].diff().dropna() / 60
@@ -26,3 +40,6 @@ periods["blocks"] = (df["height"] - 50).apply(str) + \
     "-" + (df["height"] - 1).apply(str)
 
 print(periods)
+print()
+print(
+    f"Current difficulty: {df['target'][0] / bits_to_target(current[1])} at height {current[0]}")

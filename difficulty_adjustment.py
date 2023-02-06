8ab17fb6560a43e29bc3336ac8aba9006111f370
Ubuntu <ubuntu@ip-172-31-38-63.ap-southeast-1.compute.internal>
2023-02-06 03:18:25 +0000

    plot difficulty + bpm

--- difficulty_adjustment.py
+++ difficulty_adjustment.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 from utils import bits_to_target
@@ -43,3 +44,22 @@ print(periods)
 print()
 print(
     f"Current difficulty: {df['target'][0] / bits_to_target(current[1])} at height {current[0]}")
+
+plt.figure(figsize=(12, 8))
+# Plot the difficulty as bars
+plt.bar(periods["blocks"], periods["difficulty"], width=0.8)
+plt.xlabel("Blocks")
+plt.ylabel("Difficulty")
+plt.xticks(rotation=45, ha='right', rotation_mode="anchor")
+
+# Plot the blocks per minute as a line in the same figure
+plt.twinx()
+plt.plot(periods["blocks"], periods["blocks_per_min"], color="red")
+
+plt.axhline(y=1, color="black", linestyle="--")
+plt.text(0, 1, "1 block per minute", color="black",
+         va="bottom", fontweight="bold")
+
+plt.ylabel("Blocks per minute")
+plt.title("Difficulty and blocks per minute")
+plt.savefig("difficulty_and_blocks_per_min.png")

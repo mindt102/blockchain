1aca6c21b609bb19f31b6bcd49aa282616fe29b2
mindt102 <minhdt24082002@gmail.com>
2023-02-12 17:09:31 +0700

    [mod] refactor

--- create_genesis_block.py
+++ create_genesis_block.py
@@ -1,16 +1,11 @@
 '''A script to create a genesis block'''
 
+import argparse
 import os
 
-import yaml
-
-# import utils
 from blockchain import Block, blockchain
-from Miner import miner
-# from wallet import wallet
 from database import genesis_block_path
-from utils import config
-# config = yaml.load(open('config.yml'), Loader=yaml.FullLoader)
+from Miner import miner
 
 
 def mine_genesis():
@@ -33,6 +28,15 @@ def load_genesis():
         block = Block.parse(f.read())[0]
         print(block.to_json())
 
+
+# Add -f flag to force overwrite
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--force', action='store_true')
+args = parser.parse_args()
+
+if args.force:
+    mine_genesis()
+    exit()
 
 if os.path.exists(genesis_block_path):
     print('Genesis block already exists')
